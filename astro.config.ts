db047ec756d1f76fd54bfe32f32608a83d632185
tsteele <taylor.steele@disneystreaming.com>
2023-11-02 10:53:06 -0400

    feat: add about, code block

---
+++ astro.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import expressiveCode from 'astro-expressive-code';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [expressiveCode({ theme: 'one-dark-pro' }), mdx()],
+});
