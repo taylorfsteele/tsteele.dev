3decd31ec1c3b7c05d62be3b9d008dd2a15bab00
tsteele <taylor.steele@disneystreaming.com>
2023-11-03 15:02:06 -0400

    feat: revert tailwind
    add Navigation and Footer

--- astro.config.ts
+++ astro.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import expressiveCode from 'astro-expressive-code';
 
-import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [expressiveCode({
-    theme: 'one-dark-pro'
-  }), mdx(), tailwind()]
+  integrations: [
+    expressiveCode({
+      theme: 'one-dark-pro',
+    }),
+    mdx(),
+  ],
 });
