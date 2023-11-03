b73c3ec93af2003b833fc2ce84f7e90bff260403
tsteele <taylor.steele@disneystreaming.com>
2023-11-02 20:16:47 -0400

    feat: add top nav

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import expressiveCode from 'astro-expressive-code';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [expressiveCode({ theme: 'one-dark-pro' }), mdx()],
+  integrations: [expressiveCode({
+    theme: 'one-dark-pro'
+  }), mdx(), tailwind()]
 });
---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('prettier').Config} */
+const config = {
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+  printWidth: 110,
+};
+
+export default config;
