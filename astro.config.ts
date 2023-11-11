631ff4006501448222fe83c8ca75767616cba405
tsteele <taylor.steele@disneystreaming.com>
2023-11-11 10:45:51 -0500

    feat: add fira code
    add AtomOneDark theme

--- astro.config.ts
+++ astro.config.ts
@@ -3,12 +3,16 @@ import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/static";
 import expressiveCode from "astro-expressive-code";
 import { defineConfig } from "astro/config";
+import AtomOneDark from "./AtomOneDark.json";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     expressiveCode({
-      themes: ["one-dark-pro"],
+      themes: [AtomOneDark, "nord"],
+      styleOverrides: {
+        codeFontFamily: "Fira Code Variable, sans-serif;",
+      },
     }),
     mdx(),
     react(),
